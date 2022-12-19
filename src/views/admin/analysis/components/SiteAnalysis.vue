8242ea55ff189d59ba2595db1450b5161db20941
hurt1 <55347567+hurt1@users.noreply.github.com>
2022-12-19 11:15:02 +0800

    analysis infoNum

--- src/views/admin/analysis/components/SiteAnalysis.vue
+++ src/views/admin/analysis/components/SiteAnalysis.vue
@@ -2,7 +2,7 @@
   <Card title="选择时间">
     <UseForm :updateCategory="updateCategory" />
   </Card>
-  <Card title="表格">
+  <Card title="信息数量">
     <div ref="chartRef" :style="{ height, width }"></div>
   </Card>
 </template>
@@ -11,6 +11,8 @@
   import { Card } from 'ant-design-vue';
   import UseForm from './UseForm.vue';
   import { useECharts } from '/@/hooks/web/useECharts';
+  import { infoStatisticApi } from '/@/api/demo/table';
+import { differenceInMonths } from 'date-fns';
 
   export default defineComponent({
     components: { Card, UseForm },
@@ -27,10 +29,77 @@
     setup() {
       const chartRef = ref<HTMLDivElement | null>(null);
       const { setOptions } = useECharts(chartRef as Ref<HTMLDivElement>);
-      const category = ['1'];
+      let category = [];
+      let data: number[] = [];
       const activeKey = ref('tab1');
-      const updateCategory = (data) => {
-        category.values = data;
+
+      function datedifference(sDate1, sDate2) {
+        let dateSpan, iDays;
+        sDate1 = Date.parse(sDate1);
+        sDate2 = Date.parse(sDate2);
+        dateSpan = sDate2 - sDate1;
+        dateSpan = Math.abs(dateSpan);
+        iDays = Math.floor(dateSpan / (24 * 3600 * 1000));
+        return iDays;
+      }
+
+      const updateCategory = (start, end, span, cg) => {
+        category = cg;
+        data = [];
+        category.forEach(() => {
+          data.push(0);
+        });
+        infoStatisticApi('time', span, start + 'T00:00:00', end + 'T23:59:59').then((res) => {
+          if (span == 'day') {
+            res.result.forEach((item) => {
+              let index;
+              index = datedifference(category[0], item.time.slice(0, 10));
+              data[index] = item.count;
+            });
+          } else if (span == 'week') {
+            res.result.forEach((item) => {
+              let index;
+              index = datedifference(item.time.slice(0, 10), start) / 7;
+              //console.log(item.time.slice(0, 10) + ',' + start + ',' + index);
+              data[index] = item.count;
+            });
+          } else if (span == 'month') {
+            res.result.forEach((item) => {
+              let index;
+              index = differenceInMonths(new Date(item.time.slice(0, 10)), new Date(start));
+              data[index] = item.count;
+            });
+          }
+          console.log(data);
+          setOptions({
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: {
+                lineStyle: {
+                  width: 1,
+                  color: '#019680',
+                },
+              },
+            },
+            grid: { left: '1%', right: '1%', top: '2  %', bottom: 0, containLabel: true },
+            xAxis: {
+              type: 'category',
+              data: category,
+            },
+            yAxis: {
+              type: 'value',
+              max: Math.ceil(Math.max.apply(null, data) * 1.2 + 1),
+              splitNumber: (Math.ceil(Math.max.apply(null, data) * 1.2 + 1) % 4) + 1,
+            },
+            series: [
+              {
+                data: data,
+                type: 'bar',
+                barMaxWidth: 50,
+              },
+            ],
+          });
+        });
       };
       const tabListTitle = [
         {
@@ -67,7 +136,7 @@
         },
         series: [
           {
-            data: [10, 20, 30, 40],
+            data: [],
             type: 'bar',
             barMaxWidth: 50,
           },
